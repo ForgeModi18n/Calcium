68f0159b7d33eaccc1bd44c67d0c813c4e9b55ad
KitsuneAlex <alexh@karmakrafts.dev>
2021-11-09 18:45:29 +0100

    Add mod base files & dependencies

---
+++ src/main/java/io/karma/calcium/CalciumMod.java
@@ -0,0 +1,11 @@
+package io.karma.calcium;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(CalciumMod.MODID)
+public class CalciumMod {
+    public static final String MODID = "calcium";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+}
