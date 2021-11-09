739bdbc36352a7952751f244a95913f1ca6eb43b
KitsuneAlex <alexh@karmakrafts.dev>
2021-11-09 19:24:45 +0100

    Add mixin gradle plugin & runtime test dependencies

---
+++ src/main/java/io/karma/calcium/client/mixins/BlockRendererMixin.java
@@ -0,0 +1,9 @@
+package io.karma.calcium.client.mixins;
+
+import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BlockRenderer.class)
+public class BlockRendererMixin {
+
+}
