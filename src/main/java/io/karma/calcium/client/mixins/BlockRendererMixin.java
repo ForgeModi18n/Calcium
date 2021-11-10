e57031adf9a05873a4665a066dd58a70d98ee99f
KitsuneAlex <alexh@karmakrafts.dev>
2021-11-10 01:44:13 +0100

    Don't use local capture

--- src/main/java/io/karma/calcium/client/mixins/BlockRendererMixin.java
+++ src/main/java/io/karma/calcium/client/mixins/BlockRendererMixin.java
@@ -4,14 +4,14 @@ import codechicken.lib.render.block.ICCBlockRenderer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import io.karma.calcium.CalciumMod;
 import io.karma.calcium.client.SinkingVertexBuilder;
-import me.jellysquid.mods.sodium.client.model.light.LightPipeline;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.buffers.ChunkModelBuffers;
 import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockDisplayReader;
+import net.minecraftforge.client.model.ModelDataManager;
 import net.minecraftforge.client.model.data.IModelData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,9 +19,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 import java.util.Random;
 
 @Mixin(value = BlockRenderer.class, remap = false)
@@ -32,12 +32,13 @@ public class BlockRendererMixin {
     @Shadow @Final private Random random;
     //@formatter:on
 
-    @Inject(method = "renderModel", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/renderer/model/IBakedModel;getModelData(Lnet/minecraft/world/IBlockDisplayReader;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraftforge/client/model/data/IModelData;)Lnet/minecraftforge/client/model/data/IModelData;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void onRenderModel(@Nonnull IBlockDisplayReader world, @Nonnull BlockState state, @Nonnull BlockPos pos, @Nonnull IBakedModel model, @Nonnull ChunkModelBuffers buffers, boolean cull, long seed, @Nonnull CallbackInfoReturnable<Boolean> cbi, @Nonnull LightPipeline lighter, @Nonnull Vector3d offset, @Nonnull IModelData modelData) {
+    @Inject(method = "renderModel", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/renderer/model/IBakedModel;getModelData(Lnet/minecraft/world/IBlockDisplayReader;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraftforge/client/model/data/IModelData;)Lnet/minecraftforge/client/model/data/IModelData;"), cancellable = true)
+    private void onRenderModel(@Nonnull IBlockDisplayReader world, @Nonnull BlockState state, @Nonnull BlockPos pos, @Nonnull IBakedModel model, @Nonnull ChunkModelBuffers buffers, boolean cull, long seed, @Nonnull CallbackInfoReturnable<Boolean> cbi) {
         for (final ICCBlockRenderer renderer : CalciumMod.getCustomRenderers()) {
             if (renderer.canHandleBlock(world, pos, state)) {
                 final MatrixStack mStack = matrixStack.get();
                 final SinkingVertexBuilder builder = SinkingVertexBuilder.getInstance();
+                final IModelData modelData = ModelDataManager.getModelData(Objects.requireNonNull(Minecraft.getInstance().level), pos);
 
                 mStack.clear();
 
